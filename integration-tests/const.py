59f5d2e6163159595c27451472e19f078240c2dc
asma <azinneera@gmail.com>
2018-09-11 11:56:50 +0530

    Fix review suggestions

--- integration-tests/const.py
+++ integration-tests/const.py
@@ -132,4 +132,4 @@ DB_META_DATA = {
                                 "product-is": {},
                                 "product-ei": {}}
                    }}
-MEDIATION_TEST_MODULES={"tests-mediator-1", "tests-mediator-2", "tests-service", "tests-transport", "tests-patches", "tests-sample", "tests-other"}
+MEDIATION_TEST_MODULES = {"tests-mediator-1", "tests-mediator-2", "tests-service", "tests-transport", "tests-patches", "tests-sample", "tests-other"}
